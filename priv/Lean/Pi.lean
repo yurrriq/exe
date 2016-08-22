8e353770f4336bcd2f900f59f7a2b80957931442
Namdak Tonpa <maxim@synrc.com>
2016-08-22 12:01:01 +0300

    moar baged to bages god

--- priv/Lean/Pi.lean
+++ priv/Lean/Pi.lean
@@ -8,4 +8,19 @@ constant ImperdicativePi : Π {A : Type.{a}}, (A → Type.{b}) → Type.{b}
 
 abbreviation ℿ {A : Type.{a}} := @ImperdicativePi A
 
+definition Sigma := λ (A: Type),
+                    λ (B: A -> Type),
+                    ∀ (DepProd: Type),
+                    ∀ (Make: (∀ (_1: A), ∀ (_2: B _1), DepProd) ),
+                    DepProd
+
+definition snd := λ (A: Type),
+                  λ (B: A -> Type),
+                  λ (_1: A),
+                  λ (_2: B _1),
+                  λ (P: Sigma A B),
+                  _2
+
+print snd
+
 end EXE
