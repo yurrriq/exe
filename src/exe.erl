046600fd828a11e839ef4895223333fcc17e0f47
Namdak Tonpa <maxim@synrc.com>
2016-08-25 18:45:13 +0300

    pretty printing

--- src/exe.erl
+++ src/exe.erl
@@ -63,40 +63,74 @@ mad(F)  -> case mad_repl:load_file(F) of
 
 pad(D) -> lists:duplicate(D*7," ").
 
-p(X) -> io:format("~ts~n",[unicode:characters_to_list(lists:flatten(pp(X,1)))]).
+p(X) -> io:format("~ts~n",[color(unicode:characters_to_binary(lists:flatten(pp(X,1))),[])]).
 
-color(S,gray)   -> ["\e[38;2;187;187;187m",S,"\e[0m"];
-color(S,key)    -> ["\e[36;20;52;52;200m",  S,"\e[0m"].
+ansi(S,gray)   -> ["\e[38;2;187;187;187m",S,"\e[0m"];
+ansi(S,key)    -> ["\e[36;20;52;52;200m",  S,"\e[0m"];
+ansi(S,_  )    -> S.
 
-left()     -> color("(",  gray).
-right()    -> color(")",  gray).
-arrow()    -> color(" → ",gray).
-assign()   -> color(":=", gray).
-colon()    -> color(":",  gray).
-lambda()   -> color(" λ ",  gray).
-pi()       -> color(" ∀ ",  gray).
-type()     -> color("define ",key).
-data()     -> color("data   ",key).
-record()   -> color("record ",key).
+color(<<>>,Acc) -> lists:reverse(Acc);
+color(<<"(",Rest/binary>>,Acc)      -> color(Rest,[ansi("(",gray)|Acc]);
+color(<<")",Rest/binary>>,Acc)      -> color(Rest,[ansi(")",gray)|Acc]);
+color(<<","/utf8,Rest/binary>>,Acc) -> color(Rest,[ansi(",",gray)|Acc]);
+color(<<"→"/utf8,Rest/binary>>,Acc) -> color(Rest,[ansi("→",gray)|Acc]);
+color(<<":=",Rest/binary>>,Acc)     -> color(Rest,[ansi(":=",gray)|Acc]);
+color(<<"λ"/utf8,Rest/binary>>,Acc) -> color(Rest,[ansi("λ",gray)|Acc]);
+color(<<"∀"/utf8,Rest/binary>>,Acc) -> color(Rest,[ansi("∀",gray)|Acc]);
+color(<<"record",Rest/binary>>,Acc) -> color(Rest,[ansi("record",key)|Acc]);
+color(<<"key",Rest/binary>>,Acc)    -> color(Rest,[ansi("data",key)|Acc]);
+color(<<"new",Rest/binary>>,Acc)    -> color(Rest,[ansi("new",key)|Acc]);
+color(<<"define",Rest/binary>>,Acc) -> color(Rest,[ansi("define",key)|Acc]);
+color(<<C/utf8,Rest/binary>>,Acc)   -> color(Rest,[C|Acc]).
+
+left()     -> ansi("(",    def).
+right()    -> ansi(")",    def).
+arrow()    -> ansi(" → ",  def).
+assign()   -> ansi(" := ", def).
+colon()    -> ansi(": ",   def).
+lambda()   -> ansi(" λ ",  def).
+pi()       -> ansi(" ∀ ",  def).
+type()     -> ansi("\ndefine ", def).
+data()     -> ansi("\ndata   ", def).
+new()      -> ansi("new ",      def).
+record()   -> ansi("\nrecord ", def).
+
+-define(WIDTH, 80).
+
+pc(X,P) when is_list(X) andalso length(X) > 1    -> [string:join([pp(XI,P)||XI<-X],".")];
+pc(X,P) -> pp(X,P).
 
 pp(X,P) when is_list(X) andalso length(X) > 1    -> [left(),string:join([pp(XI,P)||XI<-X]," "),right()];
 pp(X,P) when is_list(X)                          -> [string:join([pp(XI,P)||XI<-X]," ")];
-pp({assign,L,   {mk,A,B}},P) when is_list(B)     -> [pp(A,P),assign(),string:join([[left(),pp(BI,P),right()]||BI<-B],"")];
-pp({assign,L,   {mk,A,B}},P)                     -> [pp(A,P),assign(),pp(B,P)];
+pp({assign,L,   {mk,A,B}},P) when is_list(B)     -> [pp(A,P),assign(),string:join([pp(BI,P)||BI<-B]," ")];
+pp({assign,L,   {mk,A,B}},P)                     -> [type(),format_line(pp(A,P),P,?WIDTH),assign(),pp(B,P)];
 pp({preassign,L,{mk_match,[[A]],B}},P)           -> [pp(A,P)];
-pp({preassign,L,{mk_type,A}},P)                  -> [type(),[pp(A,P)]];
-pp({pretype,L,  {mk,[X],List}},P)                -> [[pp(XI,P)||XI<-X],
-                                                     string:join([[left(),pp(I,P),right(),""]||I<-List],"\n"++pad(P))
-                                                     ];
+pp({preassign,L,{mk_type,A}},P)                  -> [pp(A,P)];
+pp({pretype,L,  {mk,X,List}},P)                  -> [string:join([pp(XI,P)||XI<-X],","),
+                                                     string:join([[left(),pp(I,P),right()," "]||I<-List]," ")];
 pp({id,_,{var,X}},P)                             -> [X];
-pp({expr,L,     {mk_new,X}},P)                   -> [pp(XI,P)||XI<-X];
+pp({expr,L,     {mk_new,X}},P)                   -> [new(),[pp(XI,P)||XI<-X]];
 pp({expr,L,     {mk_define,X}},P)                -> [pp(X,P)];
-pp({expr,L,     {mk_id,X}},P)                    -> [pp(X,P)];
-pp({expr,L,     {mk_forall,Xs,Y}},P)             -> [pi(),string:join([[pad(P-1),left(),pp(X,P),right()]||X<-Xs]," ")," ",pp(Y,P)];
+pp({expr,L,     {mk_id,X}},P)                    -> [pc(X,P)];
+pp({expr,L,     {mk_forall,Xs,Y}},P)             -> [pi(),string:join([[left(),pp(X,P),right()]||X<-Xs]," "),arrow(),pp(Y,P-1)];
 pp({expr,L,     {mk_lambda,X,Y}},P)              -> [lambda(),left(),pp(X,P),right(),arrow(),pp(Y,P)];
-pp({expr,L,     {mk_data,Xs}},P)                 -> [data(),string:join([[pad(P-1),left(),pp(X,P),right()]||X<-Xs],"\n"++pad(P))];
-pp({expr,L,     {mk_record,Xs}},P)               -> [record(),string:join([[pad(P-1),left(),pp(X,P),right()]||X<-Xs],"\n"++pad(P))];
+pp({expr,L,     {mk_data,Xs}},P)                 -> [data(),string:join([[pad(P-1),left(),pp(X,P),right()]||X<-Xs],"\n"++pad(P)),"\n"];
+pp({expr,L,     {mk_record,Xs}},P)               -> [record(),format_line(string:join([[left(),[pp(X,P)],right()]||X<-Xs],"\n"++pad(P)),P+1,?WIDTH),"\n"];
 pp({expr,L,     {mk_arrow,Is,Os}},P)             -> [[pp(I,P+1)||I<-Is],arrow(),pp(Os,P)];
-pp({type,L,     {mk,I,List}},P)                  -> [pp(I,P),colon(),pp(List,P)];
-pp(Term,P) -> [].
+pp({type,L,     {mk,I,List}},P)                  -> [pc(I,P),colon(),string:join([pp(L,P)||L<-List]," ")];
+pp(Term,P)                                       -> [].
 
+format_line(S,I,P) ->
+    SSS=string:tokens(lists:flatten(S),"\n"),
+    XXX=string:join(lists:foldr(fun (SS,Acc) ->
+        FL= string:tokens(SS,"→"),
+        %io:format("format_lineA: ~tp~n",[FL]),
+        {St,_}=lists:foldr(fun(A,{L,Acc}) ->
+               AccA = length(A),
+               {String,NewAcc} = case Acc+AccA>=P of true -> {A++"\n"++pad(I),AccA};
+                                                    false -> {A,Acc+AccA} end,
+               {[String|L],NewAcc} end,{[],0},FL),
+        Joined = lists:flatten(unicode:characters_to_list(string:join(St,"→"))),
+        [Joined|Acc]
+      end,[],SSS),"\n"),
+    XXX.
